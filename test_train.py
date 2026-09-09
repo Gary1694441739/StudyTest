0f223ce8c1aad5f13cdc479fa042bec725dc7b2c
苑滔文 <yuantaowen@yuantaowendeMacBook-Air.local>
2026-09-09 21:48:15 +0800

    V0 测试版本，验证整套科研流程

---
+++ test_train.py
@@ -0,0 +1,13 @@
+import torch
+
+def main():
+    print("=======测试脚本启动=======")
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print(f"当前设备：{device}")
+    if torch.cuda.is_available():
+        print(f"GPU名称:
+        {torch.cuda.get_device_name(0)}")
+        print("测试代码运行成功，可以开始训练实验CCFB在向你招手")
+
+if __name__ == "__main__":
+    main()
