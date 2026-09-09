1c56bfc5ac3b439af9cb2db9c65b5364afd894b8
苑滔文 <yuantaowen@yuantaowendeMacBook-Air.local>
2026-09-09 22:26:35 +0800

    新加了一段numpy创造3✖️3的矩阵代码

--- test_train.py
+++ test_train.py
@@ -1,3 +1,7 @@
+import numpy as np
+A = np.array([[1,2,3],[4,5,6],[7,8,9]])
+print(A)
+
 import torch
 
 def main():
@@ -5,8 +9,7 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(f"当前设备：{device}")
     if torch.cuda.is_available():
-        print(f"GPU名称:
-        {torch.cuda.get_device_name(0)}")
+        print(f"GPU名称:{torch.cuda.get_device_name(0)}")
         print("测试代码运行成功，可以开始训练实验CCFB在向你招手")
 
 if __name__ == "__main__":
